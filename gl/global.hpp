c44acb332784280bc3dcda84b94ec3170554c49b
Themaister <maister@archlinux.us>
2013-12-21 16:29:06 +0100

    Add GLM.

--- gl/global.hpp
+++ gl/global.hpp
@@ -11,9 +11,9 @@
 #include "util.hpp"
 
 #define GLM_SWIZZLE
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 namespace GL
 {
